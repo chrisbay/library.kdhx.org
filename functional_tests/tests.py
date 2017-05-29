6cb5058bd9e90fa022c813a92f9eeaf3d7985c3b
Chris Bay <chris@chrisbay.net>
2017-05-29 11:49:20 -0500

    Run functional tests with test harness

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,19 +5,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 import os
-import unittest
 import time
+import unittest
+
+# TODO - Debug page loading issue when using StaticLiveServerTestCase
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8081'
 
 class AlbumsAppTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls.live_server_url = 'http://localhost:8000'
+        super(AlbumsAppTests, cls).setUpClass()
+
         try:
             cls.browser = webdriver.Firefox()
-            cls.browser.implicitly_wait(3)
+            cls.browser.implicitly_wait(10)
 
             # Log in
-            cls.browser.get('http://localhost:8000')
+            cls.browser.get(cls.live_server_url)
             email_field = cls.browser.find_element_by_id('identifierId')
             email_field.click()
             email_field.send_keys(os.environ['KDHX_TEST_USER_EMAIL'])
@@ -30,7 +37,7 @@ class AlbumsAppTests(unittest.TestCase):
             password_field.send_keys(Keys.RETURN)
             WebDriverWait(cls.browser, 5).until(
                 ec.visibility_of_element_located((By.ID, 'kdhx-logo')))
-            assert 'http://localhost:8000/albums/' == cls.browser.current_url, 'Login redirect failed'
+            assert cls.live_server_url+'/albums/' == cls.browser.current_url, 'Login redirect failed'
         except:
             cls.browser.quit()
             assert False, 'Login failed'
@@ -38,15 +45,16 @@ class AlbumsAppTests(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
+        super(AlbumsAppTests, cls).tearDownClass()
 
     def test_root_url_redirects_to_albums_index(self):
-        self.browser.get('http://localhost:8000')
-        self.assertEqual('http://localhost:8000/albums/', self.browser.current_url)
+        self.browser.get(self.live_server_url)
+        self.assertEqual(self.live_server_url+'/albums/', self.browser.current_url)
 
     def test_can_add_new_media(self):
 
         # User browses to /albums/media/new/ and sees the new media type form
-        self.browser.get('http://localhost:8000/albums/media/new/')
+        self.browser.get(self.live_server_url+'/albums/media/new/'new_media_url)
         self.assertIn('New Media Type', self.browser.title)
         header_text = self.browser.find_element_by_xpath('//h2[1]').text
         self.assertIn('New Media Type', header_text)
@@ -66,13 +74,11 @@ class AlbumsAppTests(unittest.TestCase):
         message_div = self.browser.find_element_by_css_selector(
             '#messages .alert-success')
         self.assertIn(label, message_div.text)
-
-        # TODO - delete type from database
     
     def test_can_add_new_album(self):
 
         # User browses to /albums/new/ and sees the new album form
-        self.browser.get('http://localhost:8000/albums/new/')
+        self.browser.get(self.live_server_url+'/albums/new/')
         self.assertIn('New Album', self.browser.title)
         header_text = self.browser.find_element_by_xpath('//h2[1]').text
         self.assertIn('New Album', header_text)
@@ -86,7 +92,3 @@ class AlbumsAppTests(unittest.TestCase):
         # TODO - User sees new album form rendered again, with a confirmation flash message
 
         # TODO - User clicks on the flash message link to view new album details
-
-
-if __name__ == '__main__':
-    unittest.main()
