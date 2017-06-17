82b9397239605d09063c429429b1b58e6fed2073
Chris Bay <chris@chrisbay.net>
2017-06-17 08:03:37 -0500

    Fix broken tests, pep8 violations

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,6 +12,7 @@ import unittest
 # from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 # os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8081'
 
+
 class AlbumsAppTests(unittest.TestCase):
 
     @classmethod
@@ -37,7 +38,8 @@ class AlbumsAppTests(unittest.TestCase):
             password_field.send_keys(Keys.RETURN)
             WebDriverWait(cls.browser, 5).until(
                 ec.visibility_of_element_located((By.ID, 'kdhx-logo')))
-            assert cls.live_server_url+'/albums/' == cls.browser.current_url, 'Login redirect failed'
+            assert cls.live_server_url+'/albums/' == cls.browser.current_url, \
+                'Login redirect failed'
         except:
             cls.browser.quit()
             assert False, 'Login failed'
@@ -49,12 +51,13 @@ class AlbumsAppTests(unittest.TestCase):
 
     def test_root_url_redirects_to_albums_index(self):
         self.browser.get(self.live_server_url)
-        self.assertEqual(self.live_server_url+'/albums/', self.browser.current_url)
+        self.assertEqual(self.live_server_url+'/albums/',
+                         self.browser.current_url)
 
     def test_can_add_new_media(self):
 
         # User browses to /albums/media/new/ and sees the new media type form
-        self.browser.get(self.live_server_url+'/albums/media/new/'new_media_url)
+        self.browser.get(self.live_server_url+'/albums/media/new/')
         self.assertIn('New Media Type', self.browser.title)
         header_text = self.browser.find_element_by_xpath('//h2[1]').text
         self.assertIn('New Media Type', header_text)
@@ -62,7 +65,7 @@ class AlbumsAppTests(unittest.TestCase):
             '//form//input[@name="label"]')
         self.assertIsNotNone(label_input)
 
-        # User enters a label for the new media type, and clicks the Save button
+        # User enters a label for the new media type, clicks the Save button
         label = 'LP-' + str(time.time())
         label_input.click()
         label_input.send_keys(label)
@@ -74,7 +77,7 @@ class AlbumsAppTests(unittest.TestCase):
         message_div = self.browser.find_element_by_css_selector(
             '#messages .alert-success')
         self.assertIn(label, message_div.text)
-    
+
     def test_can_add_new_album(self):
 
         # User browses to /albums/new/ and sees the new album form
@@ -84,11 +87,12 @@ class AlbumsAppTests(unittest.TestCase):
         self.assertIn('New Album', header_text)
         album_name_input = self.browser.find_element_by_xpath(
             '//form/input[@name="album-title"]')
-        self.assertEqual(album_name_input.get_attribute('placeholder'), 
-            'Album Title')
+        self.assertEqual(album_name_input.get_attribute('placeholder'),
+                         'Album Title')
 
         # TODO - User enters data for a new album, and clicks the Save button
 
-        # TODO - User sees new album form rendered again, with a confirmation flash message
+        # TODO - User sees new album form rendered again,
+        # with a confirmation flash message
 
-        # TODO - User clicks on the flash message link to view new album details
+        # TODO - User clicks on flash message link to view new album details
