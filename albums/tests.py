01bc1832993f1a06b13f80eb013192baafde7702
Chris Bay <chris@chrisbay.net>
2017-06-17 08:09:40 -0500

    Follow pep8

--- albums/tests.py
+++ albums/tests.py
@@ -12,7 +12,9 @@ class AlbumsTest(TestCase):
 
     @classmethod
     def remove_csrf_input(cls, html):
-        return re.sub(r'<input\stype=["\']hidden["\']\sname=["\']csrfmiddlewaretoken["\']\svalue=["\']\w+["\']\s/>', '', html)
+        return re.sub((r'<input\stype=["\']hidden["\']\sname=["\']'
+                       r'csrfmiddlewaretoken["\']\svalue=["\']\w+["\']\s/>'),
+                      '', html)
 
     def test_albums_root_resolves_to_index(self):
         found = resolve('/albums/')
@@ -21,8 +23,9 @@ class AlbumsTest(TestCase):
     def test_albums_index_returns_correct_html(self):
         request = HttpRequest()
         response = views.index(request)
-        expected_html = render_to_string('albums/index.html', 
-            {'message': 'Albums Index', 'page_title': 'Albums'})
+        view_args = {'message': 'Albums Index',
+                     'page_title': 'Albums'}
+        expected_html = render_to_string('albums/index.html', view_args)
         self.assertEqual(response.content.decode(), expected_html)
 
     def test_albums_new_resolves_to_new(self):
@@ -32,8 +35,8 @@ class AlbumsTest(TestCase):
     def test_albums_new_returns_correct_html(self):
         request = HttpRequest()
         response = views.new(request)
-        expected_html = render_to_string('albums/new.html', 
-            {'page_title': 'New Album'})
+        expected_html = render_to_string('albums/new.html',
+                                         {'page_title': 'New Album'})
         self.assertEqual(response.content.decode(), expected_html)
 
     def test_media_new_resolves_to_new(self):
@@ -47,8 +50,8 @@ class AlbumsTest(TestCase):
         response = view(request)
         found = resolve('/albums/media/new/')
         expected_html = found.func(request).rendered_content
-        self.assertEqual(self.remove_csrf_input(response.rendered_content), 
-            self.remove_csrf_input(expected_html))
+        self.assertEqual(self.remove_csrf_input(response.rendered_content),
+                         self.remove_csrf_input(expected_html))
 
     def test_can_save_new_media_type(self):
         label = 'CD'
@@ -63,4 +66,3 @@ class AlbumsTest(TestCase):
         media_type_1.save()
         media_type_2 = MediaType(label=label)
         self.assertRaises(IntegrityError, media_type_2.save)
-        
