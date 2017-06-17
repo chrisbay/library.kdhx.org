5cda05baffeec1efe9b3409900496412d3ea7420
Chris Bay <chris@chrisbay.net>
2017-06-17 09:37:06 -0500

    Remove f/e edit functionality

--- albums/urls.py
+++ albums/urls.py
@@ -5,6 +5,4 @@ app_name = 'albums'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^new/$', views.new, name='new'),
-    url(r'^media/new/$', views.MediaTypeCreate.as_view(), name='new-media'),
 ]
--- albums/views.py
+++ albums/views.py
@@ -7,24 +7,3 @@ from albums.models import MediaType
 
 def index(request):
     return render(request, 'albums/index.jinja', {'page_title': 'Albums'})
-
-
-def new(request):
-    return render(request, 'albums/new.jinja', {'page_title': 'New Album'})
-
-
-class MediaTypeCreate(SuccessMessageMixin, CreateView):
-    model = MediaType
-    template_name = 'albums/mediatype_form.jinja'
-    fields = ['label']
-    success_url = '/albums/'
-    success_message = 'New Media Type <strong>%(type_label)s</strong> created'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_title'] = 'New Media Type'
-        return context
-
-    def get_success_message(self, cleaned_data):
-        return self.success_message % dict(cleaned_data,
-                                           type_label=self.object.label)
