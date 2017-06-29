cd266d5d90db86369d39074a390c5ccf17ac6104
Chris Bay <chris@chrisbay.net>
2017-06-28 20:03:42 -0500

    Revert commit 'Remove f/e edit functionality'

--- albums/urls.py
+++ albums/urls.py
@@ -5,4 +5,6 @@ app_name = 'albums'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.new, name='new'),
+    url(r'^media/new/$', views.MediaTypeCreate.as_view(), name='new-media'),
 ]
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -53,3 +53,46 @@ class AlbumsAppTests(unittest.TestCase):
         self.browser.get(self.live_server_url)
         self.assertEqual(self.live_server_url+'/albums/',
                          self.browser.current_url)
+
+    def test_can_add_new_media(self):
+
+        # User browses to /albums/media/new/ and sees the new media type form
+        self.browser.get(self.live_server_url+'/albums/media/new/')
+        self.assertIn('New Media Type', self.browser.title)
+        header_text = self.browser.find_element_by_xpath('//h2[1]').text
+        self.assertIn('New Media Type', header_text)
+        label_input = self.browser.find_element_by_xpath(
+            '//form//input[@name="label"]')
+        self.assertIsNotNone(label_input)
+
+        # User enters a label for the new media type, clicks the Save button
+        label = 'LP-' + str(time.time())
+        label_input.click()
+        label_input.send_keys(label)
+        save_btn = self.browser.find_element_by_xpath(
+            '//form//input[@type="submit"]')
+        save_btn.click()
+
+        # User sees confirmation message in response
+        message_div = self.browser.find_element_by_css_selector(
+            '#messages .alert-success')
+        self.assertIn(label, message_div.text)
+
+    def test_can_add_new_album(self):
+
+        # User browses to /albums/new/ and sees the new album form
+        self.browser.get(self.live_server_url+'/albums/new/')
+        self.assertIn('New Album', self.browser.title)
+        header_text = self.browser.find_element_by_xpath('//h2[1]').text
+        self.assertIn('New Album', header_text)
+        album_name_input = self.browser.find_element_by_xpath(
+            '//form/input[@name="album-title"]')
+        self.assertEqual(album_name_input.get_attribute('placeholder'),
+                         'Album Title')
+
+        # TODO - User enters data for a new album, and clicks the Save button
+
+        # TODO - User sees new album form rendered again,
+        # with a confirmation flash message
+
+        # TODO - User clicks on flash message link to view new album details
