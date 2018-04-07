6f3c8329b8bd84cb0f050e2ea789df94d721cf82
Chris Bay <chris@chrisbay.net>
2018-04-07 16:15:43 -0500

    clean_albums script also deletes artists and labels

--- scripts/clean_albums.py
+++ scripts/clean_albums.py
@@ -1,7 +1,9 @@
-from albums.models import Album
+from albums.models import Album, RecordLabel, Artist
 from data.models import AlbumImport
 
 
 def run():
     Album.objects.all().delete()
+    RecordLabel.objects.all().delete()
+    Artist.objects.all().delete()
     AlbumImport.objects.all().delete()
