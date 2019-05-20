2f5494eeb7463bef4c1ef6b84296fb726fd148bc
Chris Bay <chris@chrisbay.net>
2019-05-20 16:46:24 -0500

    Moves static sources

--- library/settings.py
+++ library/settings.py
@@ -176,10 +176,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "collected-static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static-src"),
 ]
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
